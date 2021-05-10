1d3a91f720493dbc50fac733144cedba806fb33d
Sideras Jannis <it190189@EDU.HTL-LEONDING.AC.AT>
2021-05-10 10:48:03 +0200

    Scuffed

--- main.js
+++ main.js
@@ -1,71 +1,19 @@
+let id = 0;
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.querySelector("add-boardlist").addEventListener('click', function(){
+  id++;
+  let boardlist = document.createElement("div")
+  boardlist.className = "board-list " + id;
+  let listtitle = document.createElement("div")
+  listtitle.className = "list-title";
+  listtitle.innerText = "List title";
+  let addcard = document.createElement("div");
+  addcard.className = "add-card";
+  addcard.innerText = "+ Add another card";
 
-    let dragSrcEl = null;
-    
-    function handleDragStart(e) {
-      this.style.opacity = '0.6';
-      
-      dragSrcEl = this;
-  
-      e.dataTransfer.effectAllowed = 'move';
-      e.dataTransfer.setData('text/html', this.innerHTML);
-    }
-  
-    function handleDragOver(e) {
-      if (e.preventDefault) {
-        e.preventDefault();
-      }
-  
-      e.dataTransfer.dropEffect = 'move';
-      
-      return false;
-    }
-  
-    function handleDragEnter(e) {
-      this.classList.add('over');
-    }
-  
-    function handleDragLeave(e) {
-      this.classList.remove('over');
-    }
-  
-    function handleDrop(e) {
-      if (e.stopPropagation) {
-        e.stopPropagation(); // stops the browser from redirecting.
-      }
-      
-      if (dragSrcEl != this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
-      }
-      
-      return false;
-    }
-  
-    function handleDragEnd(e) {
-      this.style.opacity = '1';
-    }
-    
-    
-    let boardlist = document.querySelectorAll('.board-list');
-    boardlist.forEach(function(item) {
-        item.addEventListener('dragstart', handleDragStart, false);
-        item.addEventListener('dragenter', handleDragEnter, false);
-        item.addEventListener('dragover', handleDragOver, false);
-        item.addEventListener('dragleave', handleDragLeave, false);
-        item.addEventListener('drop', handleDrop, false);
-        item.addEventListener('dragend', handleDragEnd, false);
-    });
-    
-    let card = document.querySelectorAll('#card');
-    card.forEach(function(item) {
-        item.addEventListener('dragstart', handleDragStart, false);
-        item.addEventListener('dragenter', handleDragEnter, false);
-        item.addEventListener('dragover', handleDragOver, false);
-        item.addEventListener('dragleave', handleDragLeave, false);
-        item.addEventListener('drop', handleDrop, false);
-        item.addEventListener('dragend', handleDragEnd, false);
-    });
-});
+  boardlist.appendChild(listtitle);
+  boardlist.appendChild(addcard);
 
+  document.querySelector(".board-lists").appendChild(boardlist);
+  
+})
