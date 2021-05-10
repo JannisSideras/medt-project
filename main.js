2f63be6133a5ee1917f4e187283e9077136c5ba8
JannisSideras <siderasjannis0@gmail.com>
2021-05-11 00:40:59 +0200

    scuffed improvement

--- main.js
+++ main.js
@@ -1,19 +1,61 @@
+let addboardlist = document.createElement("div");
+addboardlist.id = "add-boardlist"
+
+let text = document.createTextNode("+ Add a list...")
+addboardlist.appendChild(text);
+
+addboardlist.onclick = function () {
+  text.parentNode.removeChild(text);
+
+  let input = document.createElement("input");
+  input.id = "input";
+  input.onkeydown = () => {
+    if (event.keyCode == 13) {
+      addBoardLists(input.value);
+    }
+  }
+
+  let add = document.createElement("button");
+  add.id = "add";
+  add.innerText = "add";
+  add.onclick = function () {
+    addBoardLists(input.value);
+    remover();
+  }
+
+  let cancel = document.createElement("button");
+  cancel.id = "cancel";
+  cancel.innerText = "X";
+  cancel.onclick = function () {
+    remover();
+  }; 
+
+  addboardlist.appendChild(input);
+  addboardlist.appendChild(add);
+  addboardlist.appendChild(cancel);
+}
+
+document.querySelector(".board-lists").appendChild(addboardlist);
+
+
+function remover () {
+  document.querySelector("#input").outerHTML = "";
+  document.querySelector("#add").outerHTML = "";
+  document.querySelector("#cancel").outerHTML = "";
+  addboardlist.appendChild(text);
+}
+
+
 let id = 0;
-document.querySelector("#add-boardlist").addEventListener('click', function(){
+function addBoardLists (listName) {
   id++;
-
-  
-
-
-
-
 
   let boardlist = document.createElement("div");
   boardlist.className = `board-list ${id}`;
 
   let listtitle = document.createElement("div");
   listtitle.className = "list-title";
-  listtitle.innerText = "List title";
+  listtitle.innerText = listName;
   
   let editList = document.createElement("input");
   editList.setAttribute("type", "text");
@@ -45,5 +87,5 @@ document.querySelector("#add-boardlist").addEventListener('click', function(){
   boardlist.appendChild(listtitle);
   boardlist.appendChild(addcard);
   document.querySelector(".board-lists").insertBefore(boardlist, document.querySelector("#add-boardlist"))
-  
-});
+
+}
